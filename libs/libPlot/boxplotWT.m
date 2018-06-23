bd5a8c34ca9afcb23ebc6e920ab93cb978cf7d34
Usman Rashid <usman@smme.edu.pk>
2018-06-23 17:06:19 +1200

    Added latex inter to boxplot

--- libs/libPlot/boxplotWT.m
+++ libs/libPlot/boxplotWT.m
@@ -1,4 +1,4 @@
-function boxplotWT(boxData, groupVector, LTitles, XTitles, placeXTitlesAt)
+function boxplotWT(boxData, groupVector, LTitles, XTitles, placeXTitlesAt, isLatex)
 %boxplotWT Box plot with titles and colors for different groups.
 %   boxData is a matrix with rows corresponding to values and columns
 %   corresponding to groups. groupVector contains group number against each
@@ -6,8 +6,12 @@ function boxplotWT(boxData, groupVector, LTitles, XTitles, placeXTitlesAt)
 %   length(XTitles) is assumed t be equal to numColumns / numGroups.
 %
 %   Copyright (c) <2016> <Usman Rashid>
-%   Licensed under the MIT License. See License.txt in the project root for 
+%   Licensed under the MIT License. See License.txt in the project root for
 %   license information.
+
+if nargin < 6
+    isLatex = 0;
+end
 
 [~, numColumns] = size(boxData);
 numGroups = length(unique(groupVector));
@@ -32,7 +36,10 @@ set(o, 'MarkerEdgeColor', medianColor);
 for j = 1:length(h)
     set(h(j), 'Color', matlabColors(groupVector(j), :)); % reordered to match
 end
-legend(h, LTitles, 'Location', 'NorthWest', 'EdgeColor', [1 1 1])
+legend(h, LTitles, 'Location', 'NorthWest', 'EdgeColor', [1 1 1]);
+if isLatex ~= 0
+    set(gca,'TickLabelInterpreter','latex');
+end
 set(gca,'TickLength',[0 0])
 set(gca, 'XTick', placeXTitlesAt);
 xticklabels(gca, XTitles);
